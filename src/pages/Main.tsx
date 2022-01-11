508683219ad6481c0161167204924f3991de8109
vgihan <jum654@naver.com>
2022-01-12 01:18:23 +0900

    feat: 메인 페이지 개발
    - 로고 영역과 로그인 입력 영역을 분리하여 해당 영역에 각 컴포넌트를
      배치함.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,20 +1,45 @@
 import styled from "@emotion/styled";
 import TextRighteous from "../components/atoms/TextRighteous";
+import LoginSpace from "../components/organisms/LoginSpace";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 60px;
+
   width: 100%;
   height: 100%;
-  background-color: black;
+  background-color: #423e3e;
 `;
+
 const Title = styled(TextRighteous)`
-  margin: 150px 0px 0px 0px;
-  font-size: 10vw;
+  font-size: 7vw;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TitleBox = styled(Box)`
+  height: 40%;
+`;
+
+const ContentBox = styled(Box)`
+  height: 20%;
 `;
 
 const Main: React.FC = () => {
   return (
     <Container>
-      <Title>O-MOK</Title>
+      <TitleBox>
+        <Title>O-MOK</Title>
+      </TitleBox>
+      <ContentBox>
+        <LoginSpace></LoginSpace>
+      </ContentBox>
     </Container>
   );
 };
