0dcc16d1d67e1abab45df3e3c597576231b86d32
vgihan <jum654@naver.com>
2022-01-12 01:17:51 +0900

    feat: 메인 페이지 로그인 입력 organisms 개발

---
+++ src/components/organisms/LoginSpace.tsx
@@ -0,0 +1,72 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import TextInput from "../atoms/TextInput";
+import TextRighteous from "../atoms/TextRighteous";
+import ButtonColorless from "../atoms/ButtonColorless";
+
+const InputLabel = styled(TextRighteous)`
+  font-size: 30px;
+  height: 30px;
+`;
+
+const Line = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 400px;
+  height: 60px;
+`;
+
+const LoginInput = styled(TextInput)`
+  font-size: 30px;
+  width: 275px;
+  height: 100%;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 25px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 30px;
+`;
+
+const LoginSpace: React.FC = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  return (
+    <Container>
+      <InputContainer>
+        <Line>
+          <InputLabel>ID</InputLabel>
+          <LoginInput className="id" setInput={setId} input={id}></LoginInput>
+        </Line>
+        <Line>
+          <InputLabel>PW</InputLabel>
+          <LoginInput
+            className="pw"
+            setInput={setPw}
+            input={pw}
+            isPassword
+          ></LoginInput>
+        </Line>
+      </InputContainer>
+      <ButtonContainer>
+        <ButtonColorless>Login</ButtonColorless>
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default LoginSpace;
