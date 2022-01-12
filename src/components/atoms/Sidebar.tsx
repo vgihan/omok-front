044c731500bb78b4815328f7eaa01dd742f73657
vgihan <jum654@naver.com>
2022-01-12 19:19:30 +0900

    feat: add SidebarBoxTitle
    - div 박스 height를 2px로 주어 선 처럼 보이게 구현함.

--- src/components/atoms/Sidebar.tsx
+++ src/components/atoms/Sidebar.tsx
@@ -1,13 +1,15 @@
 import styled from "@emotion/styled";
+import { PropsWithChildren } from "react";
 import theme from "../../styles/theme";
 
 const Container = styled.div<Props>`
   background-color: ${theme.colors.cottonWhite};
   border-radius: 30px;
-  width: 380px;
+  width: 300px;
   height: 100%;
   left: ${({ isActive }) => (isActive ? "0" : "-100%")};
   position: absolute;
+  padding: 50px;
   box-shadow: 2px 2px 20px 4px rgba(0, 0, 0, 0.3);
   animation: 0.7s ${({ isActive }) => (isActive ? "showUp" : "showOut")};
 
@@ -34,8 +36,11 @@ type Props = {
   isActive: boolean;
 };
 
-const Sidebar: React.FC<Props> = ({ isActive }: Props) => {
-  return <Container isActive={isActive} />;
+const Sidebar: React.FC<PropsWithChildren<Props>> = ({
+  isActive,
+  children,
+}: PropsWithChildren<Props>) => {
+  return <Container isActive={isActive}>{children}</Container>;
 };
 
 export default Sidebar;
