7a730712d475c7e8c3d2591bd913b865de3fa2f0
vgihan <jum654@naver.com>
2022-01-13 18:43:40 +0900

    feat: add Lobby Page
    - organisms를 조합하여 구성

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,10 +1,14 @@
 import styled from "@emotion/styled";
+import useOutsideClick from "../hooks/useOutsideClick";
 import ButtonRound from "../components/atoms/ButtonRound";
 import TextRighteous from "../components/atoms/TextRighteous";
 import Topbar from "../components/molecules/Topbar";
 import Sidebar from "../components/atoms/Sidebar";
+import SidebarProfileBox from "../components/organisms/SidebarProfileBox";
+import SidebarRankBox from "../components/organisms/SidebarRankBox";
+import SidebarCommunityBox from "../components/organisms/SidebarCommunityBox";
 import theme from "../styles/theme";
-import { useState } from "react";
+import { useRef } from "react";
 
 const TitleContainer = styled.div`
   display: flex;
@@ -16,32 +20,67 @@ const TitleContainer = styled.div`
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
-  padding: 0px 15% 0px 15%;
+  justify-content: center;
+  column-gap: 10vw;
 `;
 
 const PlayText = styled(TextRighteous)`
   font-size: 150px;
+  @media (max-width: ${theme.breakpoints.lg}) {
+    font-size: 150px;
+  }
+  @media (max-width: ${theme.breakpoints.md}) {
+    font-size: 100px;
+  }
 `;
 
 const SubText = styled(TextRighteous)`
-  font-size: 50px;
+  font-size: 70px;
+  @media (max-width: ${theme.breakpoints.lg}) {
+    font-size: 70px;
+  }
+  @media (max-width: ${theme.breakpoints.md}) {
+    font-size: 50px;
+  }
 `;
 
 const ModeButton = styled(ButtonRound)`
   color: black;
   background-color: ${theme.colors.lightGray};
-  width: 15vw;
-  height: 15vw;
+  font-size: 40px;
+  width: 230px;
+  height: 230px;
+  @media (max-width: ${theme.breakpoints.lg}) {
+    font-size: 40px;
+    width: 230px;
+    height: 230px;
+  }
+  @media (max-width: ${theme.breakpoints.md}) {
+    font-size: 25px;
+    width: 130px;
+    height: 130px;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    font-size: 15px;
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 const Lobby: React.FC = () => {
-  const [sidebarSwitch, setSidebarSwitch] = useState(false);
+  const TopbarRef = useRef(null);
+  const { isClicked, setIsClicked } = useOutsideClick(TopbarRef);
 
   return (
     <>
-      <Sidebar isActive={sidebarSwitch}></Sidebar>
-      <Topbar onClickMenu={() => setSidebarSwitch((prev) => prev !== true)} />
+      <div ref={TopbarRef}>
+        <Sidebar isActive={isClicked}>
+          <SidebarProfileBox />
+          <SidebarRankBox />
+          <SidebarCommunityBox />
+        </Sidebar>
+      </div>
+      <Topbar onClickMenu={() => setIsClicked(true)} />
       <TitleContainer>
         <PlayText>PLAY</PlayText>
         <SubText>O-MOK</SubText>
