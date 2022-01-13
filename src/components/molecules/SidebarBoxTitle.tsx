43f774f5d0fe2553c4c70d7e736caeddd79af0d6
vgihan <jum654@naver.com>
2022-01-13 18:31:00 +0900

    fix: SidebarBoxTitle 반응형 개선

--- src/components/molecules/SidebarBoxTitle.tsx
+++ src/components/molecules/SidebarBoxTitle.tsx
@@ -6,16 +6,26 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 20px;
   background-color: ${theme.colors.lightGray};
+  width: 100%;
   height: 2px;
+  margin: 0px 0px 60px 0px;
+  @media (max-width: ${theme.breakpoints.lg}) {
+    margin: 0px 0px 60px 0px;
+  }
+  @media (max-width: ${theme.breakpoints.md}) {
+    margin: 0px 0px 40px 0px;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    margin: 0px 0px 20px 0px;
+  }
 `;
 
 const WordBox = styled(TextRighteous)`
   background-color: ${theme.colors.cottonWhite};
   padding: 10px;
   color: black;
-  font-size: 30px;
+  font-size: 3vmin;
 `;
 
 const SidebarBoxTitle: React.FC = ({ children }) => {
