6db90dc04d96855989bbd11944c00a96be949790
vgihan <jum654@naver.com>
2022-01-13 17:42:06 +0900

    fix: atoms component 반응형 개선
    - media query 를 이용한 반응형 개선

--- src/components/atoms/Sidebar.tsx
+++ src/components/atoms/Sidebar.tsx
@@ -3,32 +3,29 @@ import { PropsWithChildren } from "react";
 import theme from "../../styles/theme";
 
 const Container = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+  row-gap: 50px;
   background-color: ${theme.colors.cottonWhite};
-  border-radius: 30px;
-  width: 300px;
-  height: 100%;
-  left: ${({ isActive }) => (isActive ? "0" : "-100%")};
+  border-radius: 3vmin;
+  left: ${({ isActive }) => (isActive ? "0" : "-150%")};
   position: absolute;
-  padding: 50px;
+  padding: 40px;
+  width: 340px;
   box-shadow: 2px 2px 20px 4px rgba(0, 0, 0, 0.3);
-  animation: 0.7s ${({ isActive }) => (isActive ? "showUp" : "showOut")};
+  transition: all 0.5s;
 
-  @keyframes showUp {
-    0% {
-      transform: translate(-100%, 0);
-    }
-    100% {
-      transform: translate(0, 0);
-    }
+  @media (max-width: ${theme.breakpoints.lg}) {
+    width: 340px;
+    padding: 40px;
   }
-  @keyframes showOut {
-    0% {
-      transform: translate(0, 0);
-    }
-    100% {
-      transform: translate(-100%, 0);
-      display: none;
-    }
+  @media (max-width: ${theme.breakpoints.md}) {
+    width: 200px;
+    padding: 40px;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    width: 130px;
+    padding: 20px;
   }
 `;
 
@@ -40,6 +37,7 @@ const Sidebar: React.FC<PropsWithChildren<Props>> = ({
   isActive,
   children,
 }: PropsWithChildren<Props>) => {
+  console.log(isActive);
   return <Container isActive={isActive}>{children}</Container>;
 };
 
