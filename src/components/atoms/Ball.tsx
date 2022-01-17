dc13c248b92391b6a7cbf7a03e9c8a2e55be9399
vgihan <jum654@naver.com>
2022-01-17 18:23:19 +0900

    feat: add ball, centerbox components
    - 바둑알 컴포넌트 ball 구현
    - flex display를 이용한 가운데 정렬 박스 구현

---
+++ src/components/atoms/Ball.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+import theme from "../../styles/theme";
+
+type Props = {
+  color: "white" | "black";
+};
+
+const Ball = styled.span`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: ${theme.colors.cottonWhite};
+  background: ${({ color }: Props) =>
+    color === "white"
+      ? "linear-gradient(to bottom right, white 45%, #555);"
+      : "linear-gradient(-41deg, black, #363636 60%, #999);"};
+`;
+
+export default Ball;
---
+++ src/components/atoms/CenterBox.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const CenterBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default CenterBox;
