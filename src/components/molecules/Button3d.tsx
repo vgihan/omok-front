d2582e25d9d4b40d9b44f29c87c46b1df46c5bbb
vgihan <jum654@naver.com>
2022-01-18 18:54:15 +0900

    feat: 플레이 시 착수 버튼 구현
    - div 태그를 겹쳐 3d 처럼 보이도록 구현함.
    - 클릭 시 겹쳐진 div 태그를 오른쪽 아래로 10px 이동함.

---
+++ src/components/molecules/Button3d.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { PropsWithChildren } from "react";
+import theme from "../../styles/theme";
+import ButtonRound from "../atoms/ButtonRound";
+
+const Container = styled(ButtonRound)`
+  border-radius: 6%;
+  font-family: roboto;
+  font-weight: bold;
+  color: ${theme.colors.gray};
+  background-color: black;
+`;
+
+const Wrapper = styled.div<Props>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.colors.opacityCottonWhite};
+  color: ${theme.colors.gray};
+  position: relative;
+  border-radius: 6%;
+  width: ${({ width }) => width}vmin;
+  height: ${({ height }) => height}vmin;
+  font-size: ${({ fontSize }) => fontSize}vmin;
+  left: -5px;
+  top: -5px;
+  :active {
+    left: 5px;
+    top: 5px;
+  }
+`;
+
+type Props = {
+  width: string;
+  height: string;
+  fontSize: string;
+};
+
+const Button: React.FC<Props> = (props: PropsWithChildren<Props>) => {
+  return (
+    <Container>
+      <Wrapper {...props}>{props.children}</Wrapper>
+    </Container>
+  );
+};
+
+export default Button;
