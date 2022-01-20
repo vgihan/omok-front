fe8d9f04750d4776a6d6c0d84f0a8dd3f5ffd5af
vgihan <jum654@naver.com>
2022-01-21 03:17:25 +0900

    fix: ChatSpace 단에서 채팅 기록을 관리하도록 수정
    - 전역 상태 관리하기 이전에 임시로 ChatSpace 단에서 관리하도록 함.

--- src/components/organisms/ChatSpace.tsx
+++ src/components/organisms/ChatSpace.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import { Chat } from "../../types/chat";
 import ChatInputBox from "../molecules/ChatInputBox";
 import ChatRecordBox from "../molecules/ChatRecordBox";
 
 const ChatSpace: React.FC = () => {
+  const [chats, setChats] = useState<Chat[]>([]);
+
+  const sendChat = (chat: Chat) => {
+    setChats((prevState: Chat[]) => [chat, ...prevState]);
+  };
+
   return (
     <>
-      <ChatRecordBox />
-      <ChatInputBox />
+      <ChatRecordBox chats={chats} />
+      <ChatInputBox sendChat={sendChat} />
     </>
   );
 };
