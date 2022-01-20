9f08d7138de566e374e73656bc42518c28a058a7
vgihan <jum654@naver.com>
2022-01-21 01:23:32 +0900

    fix: ChatRecordBox 실제 채팅 내용을 담을 수 있도록 수정
    - Topbar height 단위 변경

--- src/components/molecules/ChatRecordBox.tsx
+++ src/components/molecules/ChatRecordBox.tsx
@@ -1,7 +1,12 @@
 import styled from "@emotion/styled";
 import theme from "../../styles/theme";
+import ChatElement from "../atoms/ChatElement";
+import { Chat } from "../../types/chat";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: ${theme.colors.darkGray};
   box-sizing: border-box;
   opacity: 54;
@@ -16,10 +21,52 @@ const Line = styled.div`
   opacity: 50%;
 `;
 
-const ChatRecordBox: React.FC = () => {
+const RecordContainer = styled.ul`
+  -webkit-mask-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 1) 20%
+  );
+  -webkit-mask-size: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  row-gap: 6px;
+  overflow-y: auto;
+`;
+
+const AllChat = styled(ChatElement)`
+  color: ${theme.colors.gray};
+`;
+
+const TeamChat = styled(ChatElement)`
+  color: ${theme.colors.aqua};
+  :before {
+    content: "[Team] ${({ sender }) => sender} : ";
+  }
+`;
+
+type Props = {
+  chats: Chat[];
+};
+
+const ChatRecordBox: React.FC<Props> = ({ chats }: Props) => {
   return (
     <Container>
       <Line />
+      <RecordContainer>
+        {chats.map((chat, idx) =>
+          chat.type === "all" ? (
+            <AllChat key={idx} sender={chat.sender}>
+              {chat.content}
+            </AllChat>
+          ) : (
+            <TeamChat key={idx} sender={chat.sender}>
+              {chat.content}
+            </TeamChat>
+          )
+        )}
+      </RecordContainer>
     </Container>
   );
 };
