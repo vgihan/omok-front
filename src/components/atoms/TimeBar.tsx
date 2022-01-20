af563d6deed411a68106215d77f8037013a809f8
vgihan <jum654@naver.com>
2022-01-20 20:45:31 +0900

    feat: add TimeBar
    - keyframe을 통해 렌더링 이후 바로 애니메이션이 시작되도록 구현함.

---
+++ src/components/atoms/TimeBar.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+import theme from "../../styles/theme";
+
+const Container = styled.div`
+  border: 2px solid ${theme.colors.lightGray};
+  width: 80vmin;
+  height: 12px;
+  box-sizing: border-box;
+  margin-top: -4vmin;
+`;
+
+const ProgressBox = styled.div<{ time: number }>`
+  width: 100%;
+  height: 100%;
+  background: ${theme.colors.lightGray};
+  animation: timeFlow 3s linear forwards;
+  @keyframes timeFlow {
+    from {
+      width: 100%;
+    }
+    to {
+      width: 0%;
+    }
+  }
+`;
+
+const TimeBar: React.FC = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("end !!");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Container>
+      <ProgressBox time={30} />
+    </Container>
+  );
+};
+
+export default TimeBar;
