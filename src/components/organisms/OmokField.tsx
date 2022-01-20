57cea8d2ca280ae3c1c35b65b8e29d5662e8688c
vgihan <jum654@naver.com>
2022-01-20 16:15:01 +0900

    fix: add OmokField (Svg & Svg Version)
    - line과 바둑돌 box 둘 다 svg를 이용하여 구현

--- src/components/organisms/OmokField.tsx
+++ src/components/organisms/OmokField.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { MouseEventHandler, useState } from "react";
-import CrossShape from "../atoms/CrossShape";
+import theme from "../../styles/theme";
 
 const SvgBoard = styled.svg`
   height: 100%;
@@ -8,27 +8,49 @@ const SvgBoard = styled.svg`
   cursor: pointer;
 `;
 
+const Line = styled.line`
+  stroke: #000;
+  stroke-width: 1;
+`;
+
+const Rect = styled.rect`
+  fill: transparent;
+  :hover {
+    stroke: ${theme.colors.opacityCottonWhite};
+    stroke-width: 5px;
+  }
+  :active {
+    stroke: black;
+    stroke-width: 5px;
+  }
+`;
+
+const ONE_SPACE = 50;
+const END_COORDINATE = 750;
+const CORRECTION_LINE_CONSTANT = 0.5;
+const FOCUS_BOX_SIZE = 32;
+
 type BallInfo = {
   type: "black" | "white";
-  pos: {
-    x: number;
-    y: number;
-  };
+  pos: Pos;
+};
+
+type Pos = {
+  x: number;
+  y: number;
 };
 
 const OmokField: React.FC = () => {
-  const init: BallInfo[] = [
-    { type: "black", pos: { x: 1, y: 1 } },
-    { type: "white", pos: { x: 1, y: 2 } },
-    { type: "white", pos: { x: 2, y: 1 } },
-    { type: "black", pos: { x: 3, y: 4 } },
-  ];
-  const [ballsInfo, setBallsInfo] = useState<BallInfo[]>(init);
-  const unit = Array.from({ length: 13 }).map((v, i) => (i + 2) * 50);
-  const coordinate = Array.from({ length: 13 }).map((col) => [...unit]);
+  const [ballsInfo, setBallsInfo] = useState<BallInfo[]>([]);
+  const [selected, setSelected] = useState<Pos | null>(null);
+  const coordinate = Array.from({ length: 15 }).map((v, i) => i + 1);
 
   const handleClickField: MouseEventHandler = (e) => {
-    console.log(e.clientX);
+    const x = e.currentTarget.getAttribute("data-x");
+    const y = e.currentTarget.getAttribute("data-y");
+    if (!x || !y) return;
+
+    setSelected({ x: parseInt(x), y: parseInt(y) });
   };
 
   return (
@@ -50,16 +72,49 @@ const OmokField: React.FC = () => {
       <circle cx="200" cy="600" r="6" fill="black" />
       <circle cx="600" cy="200" r="6" fill="black" />
       <circle cx="600" cy="600" r="6" fill="black" />
-
-      {coordinate.map((arr, row) =>
-        arr.map((pos) => (
-          <CrossShape center={{ x: pos, y: (row + 2) * 50 + 0.5 }} />
-        ))
+      {coordinate.map((pos) => (
+        <Line
+          key={"x" + pos}
+          x1={pos * ONE_SPACE + CORRECTION_LINE_CONSTANT}
+          y1={ONE_SPACE + CORRECTION_LINE_CONSTANT}
+          x2={pos * ONE_SPACE + CORRECTION_LINE_CONSTANT}
+          y2={END_COORDINATE + CORRECTION_LINE_CONSTANT}
+        />
+      ))}
+      {coordinate.map((pos) => (
+        <Line
+          key={"y" + pos}
+          x1={ONE_SPACE + CORRECTION_LINE_CONSTANT}
+          y1={pos * ONE_SPACE + CORRECTION_LINE_CONSTANT}
+          x2={END_COORDINATE + CORRECTION_LINE_CONSTANT}
+          y2={pos * ONE_SPACE + CORRECTION_LINE_CONSTANT}
+        />
+      ))}
+      {coordinate.map((x) =>
+        coordinate.map((y) => {
+          const isSelected = selected && selected.x === x && selected.y === y;
+          return (
+            <Rect
+              key={`${x},${y}`}
+              data-x={x}
+              data-y={y}
+              width={FOCUS_BOX_SIZE}
+              height={FOCUS_BOX_SIZE}
+              x={x * ONE_SPACE + CORRECTION_LINE_CONSTANT - FOCUS_BOX_SIZE / 2}
+              y={y * ONE_SPACE + CORRECTION_LINE_CONSTANT - FOCUS_BOX_SIZE / 2}
+              rx={3}
+              ry={3}
+              onClick={handleClickField}
+              stroke={isSelected ? "black" : ""}
+              strokeWidth={isSelected ? "5px" : ""}
+            />
+          );
+        })
       )}
       {ballsInfo.map((info) => (
         <circle
-          cx={`${info.pos.x * 50}`}
-          cy={`${info.pos.y * 50}`}
+          cx={`${info.pos.x * ONE_SPACE}`}
+          cy={`${info.pos.y * ONE_SPACE}`}
           r="23"
           fill={`url('#${info.type}')`}
         />
