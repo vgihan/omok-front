8ae3caf175a4f6c37852c03886b58ce62e623176
vgihan <jum654@naver.com>
2022-01-24 16:47:48 +0900

    fix: modify TextRighteous
    - css 폰트 속성만 가지고 있도록 역할 축소시킴

--- src/components/atoms/TextRighteous.tsx
+++ src/components/atoms/TextRighteous.tsx
@@ -2,10 +2,7 @@ import styled from "@emotion/styled";
 import theme from "../../styles/theme";
 
 const TextRighteous = styled.p`
-  display: flex;
-  justify-content: center;
   font-family: ${theme.fonts.righteous};
-  color: white;
 `;
 
 export default TextRighteous;
