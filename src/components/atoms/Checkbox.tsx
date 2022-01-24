ee57c65eb754ffbdc8314bc9b8dd3ba9510a54c9
vgihan <jum654@naver.com>
2022-01-24 22:53:31 +0900

    feat: add Checkbox

---
+++ src/components/atoms/Checkbox.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import { ChangeEventHandler } from "react";
+import theme from "../../styles/theme";
+import CenterBox from "./CenterBox";
+
+const Container = styled.label`
+  margin: 0 1vw;
+  cursor: pointer;
+`;
+
+const InputCheck = styled.input`
+  display: none;
+`;
+
+const FillBox = styled(CenterBox)`
+  background-color: ${theme.colors.lightCharcoal};
+  width: 35px;
+  height: 35px;
+  color: ${theme.colors.darkGray};
+  font-size: 35px;
+`;
+
+type Props = {
+  isChecked: boolean;
+  onChange: ChangeEventHandler;
+};
+
+const Checkbox: React.FC<Props> = ({ isChecked, onChange }) => {
+  return (
+    <Container>
+      <InputCheck type="checkbox" checked={isChecked} onChange={onChange} />
+      <FillBox>{isChecked ? "✔" : ""}</FillBox>
+    </Container>
+  );
+};
+
+export default Checkbox;
