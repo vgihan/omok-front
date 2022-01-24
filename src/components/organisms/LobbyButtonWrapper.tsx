e70436d86e87db3a564e50dc2aa0a526e5474d75
vgihan <jum654@naver.com>
2022-01-24 23:02:24 +0900

    feat: add lobby element
    - 랭크 게임, 방만들기, search bar Wrapper 구현
    - RoomList, RoomUnit 구현

---
+++ src/components/organisms/LobbyButtonWrapper.tsx
@@ -0,0 +1,62 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+import theme from "../../styles/theme";
+import ButtonRound from "../atoms/ButtonRound";
+import Modal from "../atoms/Modal";
+import Portal from "../atoms/Portal";
+import ModalRoomMake from "./ModalRoomMake";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 5%;
+  width: 100%;
+  height: 100%;
+`;
+
+const ModeButton = styled(ButtonRound)`
+  background-color: ${theme.colors.opacityCottonWhite};
+  color: ${theme.colors.darkGray};
+  height: 35%;
+  font-size: 7vmin;
+  border-radius: 1vmin;
+  :hover {
+    background-color: ${theme.colors.lightGray};
+  }
+`;
+
+const SearchBox = styled.input`
+  background-color: ${theme.colors.opacityCottonWhite};
+  color: ${theme.colors.darkGray};
+  height: 10%;
+  font-size: 2vmin;
+  border-radius: 1vmin;
+  margin-bottom: 10%;
+  background-image: url("img/search.svg");
+  background-size: 8%;
+  background-repeat: no-repeat;
+  background-position: left;
+  background-position-x: 2%;
+  padding: 0 0 0 13%;
+  outline: none;
+`;
+
+const LobbyButtonWrapper: React.FC = () => {
+  const [isOpenedModal, setIsOpenedModal] = useState<boolean>(false);
+  return (
+    <Container>
+      {isOpenedModal && (
+        <Portal>
+          <Modal>
+            <ModalRoomMake handleOffModal={() => setIsOpenedModal(false)} />
+          </Modal>
+        </Portal>
+      )}
+      <SearchBox placeholder="Search" />
+      <ModeButton>Rank mode</ModeButton>
+      <ModeButton onClick={() => setIsOpenedModal(true)}>new room</ModeButton>
+    </Container>
+  );
+};
+
+export default LobbyButtonWrapper;
---
+++ src/components/organisms/LobbyRoomList.tsx
@@ -0,0 +1,101 @@
+import styled from "@emotion/styled";
+import { Room } from "../../types/room";
+import LobbyRoomUnit from "./LobbyRoomUnit";
+import theme from "../../styles/theme";
+import CenterBox from "../atoms/CenterBox";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 27vw 27vw;
+  grid-template-rows: 15vh 15vh 15vh 10vh;
+  grid-gap: 1vmin 1vmin;
+`;
+
+const ButtonContainer = styled(CenterBox)`
+  grid-column: 1/3;
+  column-gap: 1vmin;
+  width: 100%;
+`;
+
+const PageButton = styled.button`
+  width: 0px;
+  height: 0px;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const PrevPageButton = styled(PageButton)`
+  border-right: 30px solid ${theme.colors.opacityCottonWhite};
+  :hover {
+    border-right: 30px solid ${theme.colors.lightGray};
+  }
+`;
+
+const NextPageButton = styled(PageButton)`
+  border-left: 30px solid ${theme.colors.opacityCottonWhite};
+  :hover {
+    border-left: 30px solid ${theme.colors.lightGray};
+  }
+`;
+
+const LobbyRoomList: React.FC = () => {
+  const rooms: Room[] = [
+    {
+      id: "01",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+    {
+      id: "02",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+    {
+      id: "03",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+    {
+      id: "04",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+    {
+      id: "05",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+    {
+      id: "06",
+      isLock: true,
+      mode: "1 vs 1",
+      name: "Room Name",
+      state: "WAITING",
+    },
+  ];
+  return (
+    <Container>
+      {rooms.map((room) => (
+        <LobbyRoomUnit {...room} key={room.id} />
+      ))}
+      <ButtonContainer>
+        <PrevPageButton />
+        <NextPageButton />
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default LobbyRoomList;
