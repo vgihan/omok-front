cceb663f4489c558508bd4782e39d95e6dafb131
vgihan <jum654@naver.com>
2022-01-24 16:47:10 +0900

    feat: add TextRoboto
    - Roboto 폰트를 사용하는 p 태그 컴포넌트 추가

---
+++ src/components/atoms/TextRoboto.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import theme from "../../styles/theme";
+
+const TextRighteous = styled.p`
+  font-family: ${theme.fonts.righteous};
+`;
+
+export default TextRighteous;
