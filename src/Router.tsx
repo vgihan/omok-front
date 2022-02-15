042cac4012cf5b5642ac1148f720c45531d612f5
vgihan <jum654@naver.com>
2022-02-15 18:41:06 +0900

    feat: Join 페이지 구현
    - Router에 Join 페이지 적용
    - Main 페이지에서 Join 버튼 클릭 시 이동하도록 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Main from "./pages/Main";
 import Lobby from "./pages/Lobby";
 import Play from "./pages/Play";
-import { BrowserRouter } from "react-router-dom";
+import Join from "./pages/Join";
 
 const Router: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Router: React.FC = () => {
         <Route path="/" element={<Main />} />
         <Route path="/lobby" element={<Lobby />} />
         <Route path="/play" element={<Play />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
