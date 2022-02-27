b6818e983bc4f14e16803cb804e4220dfd05210f
vgihan <jum654@naver.com>
2022-02-28 00:36:50 +0900

    feat: react-query 사용을 위한 QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Global, ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import React from "react";
 import Router from "./Router";
 import reset from "./styles/reset";
 import theme from "./styles/theme";
 import "./styles/font.css";
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={reset} />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={reset} />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
