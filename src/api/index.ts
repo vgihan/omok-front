3c96a8d963e87fff397e347f2a69633eb6a43401
vgihan <jum654@naver.com>
2022-02-28 00:28:40 +0900

    feat: add useSignin api function
    - 로그인을 위한 api function을 추상화함.

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { QueryKey, useQuery, UseQueryOptions } from "react-query";
+import { ErrorResponse, SigninResponse } from "../types/signinResponse";
+
+export const useSignin = (
+  data: { id: string; password: string },
+  options?: Omit<
+    UseQueryOptions<
+      AxiosResponse<SigninResponse>,
+      AxiosError<ErrorResponse>,
+      QueryKey,
+      "signin"
+    >,
+    "queryKey"
+  >
+) => {
+  return useQuery(
+    "signin",
+    () => axios.post("http://localhost:5000/auth/signin", data),
+    options
+  );
+};
