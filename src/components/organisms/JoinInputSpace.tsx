d37f78a572157a60e5dca8f9d44bee6100a64212
vgihan <jum654@naver.com>
2022-03-15 20:11:31 +0900

    feat: 회원가입 페이지 구현
    - pw 와 checkPW가 다를 경우 실시간으로 경고 메시지가 뜨도록 구현

--- src/components/organisms/JoinInputSpace.tsx
+++ src/components/organisms/JoinInputSpace.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
 
+import { useFetchSignup } from "~api/index";
 import ButtonRound from "~components/atoms/ButtonRound";
+import TextRoboto from "~components/atoms/TextRoboto";
 import LabelTextInput from "~components/molecules/LabelTextInput";
 
 const Container = styled.div`
@@ -20,23 +23,52 @@ const JoinButton = styled(ButtonRound)`
   font-size: 50px;
   margin: 20px 0 0 0;
 `;
+const InputState = styled(TextRoboto)`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
 const JoinInputSpace: React.FC = () => {
   const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const [password, setPassword] = useState("");
   const [checkedPw, setCheckedPw] = useState("");
+
+  const navigate = useNavigate();
+
+  const { refetch } = useFetchSignup(
+    { id, password },
+    {
+      enabled: false,
+      onSuccess: () => {
+        alert("로그인에 성공했습니다 ! 새로 로그인 해주세요 ^_^");
+        navigate("/");
+      },
+      onError: (err) => {
+        alert(err.response?.data.message);
+      },
+    },
+  );
+
+  const handleClickJoinBtn = () => {
+    refetch();
+  };
+
   return (
     <Container>
       <JoinInput value={id} onChange={(e) => setId(e.currentTarget.value)}>
         ID
       </JoinInput>
-      <JoinInput type="password" value={pw} onChange={(e) => setPw(e.currentTarget.value)}>
+      <JoinInput type="password" value={password} onChange={(e) => setPassword(e.currentTarget.value)}>
         PW
       </JoinInput>
       <JoinInput type="password" value={checkedPw} onChange={(e) => setCheckedPw(e.currentTarget.value)}>
         Check PW
       </JoinInput>
-      <JoinButton>Join</JoinButton>
+      <InputState>{password && checkedPw && password !== checkedPw && "비밀번호와 동일하게 입력하세요"}</InputState>
+      <JoinButton disabled={!password || password !== checkedPw} onClick={handleClickJoinBtn}>
+        Join
+      </JoinButton>
     </Container>
   );
 };
