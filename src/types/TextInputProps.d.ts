570d37d970afd2b2e69848524dd13c70ac41ec6c
vgihan <jum654@naver.com>
2022-03-15 16:24:27 +0900

    fix: delete TextInputProps

--- src/types/TextInputProps.d.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatch, KeyboardEventHandler, SetStateAction } from "react";
-
-export interface TextInputProps {
-  setInput: Dispatch<SetStateAction<string>>;
-  input: string;
-  className?: string;
-  isPassword?: boolean;
-  onKeyUp?: KeyboardEventHandler;
-}
