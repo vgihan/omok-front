b2b38b9cbcec06a3a16cdc8d9915a49d36cff853
vgihan <jum654@naver.com>
2022-03-16 21:18:09 +0900

    chore: queries alias 추가

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,7 @@ module.exports = {
       "~components": path.resolve(__dirname, "src/components"),
       "~hooks": path.resolve(__dirname, "src/hooks"),
       "~pages": path.resolve(__dirname, "src/pages"),
+      "~queries": path.resolve(__dirname, "src/queries"),
       "~styles": path.resolve(__dirname, "src/styles"),
       "~types": path.resolve(__dirname, "src/types"),
       "~utils": path.resolve(__dirname, "src/utils"),
