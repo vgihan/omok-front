5bb848b54a0b8f3e1378d8e04f46a0e8ba28a1d3
vgihan <jum654@naver.com>
2022-03-18 15:59:57 +0900

    chore: add room path in proxy config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    ["/user", "/auth"],
+    ["/user", "/auth", "/room"],
     createProxyMiddleware({
       target: process.env.REACT_APP_API_SERVER,
       changeOrigin: true,
