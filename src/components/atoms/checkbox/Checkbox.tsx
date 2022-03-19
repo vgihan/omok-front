f4dcbed4a17d7a34e292a7eda5c2ff09febc3c0f
vgihan <jum654@naver.com>
2022-03-19 23:59:53 +0900

    refactor: checkbox 컴포넌트 리팩토링
    - children으로 체크표시를 나타내던 기존 방식에서 css :after 요소를
      이용하여 적용

--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -1,25 +1,21 @@
-import { MouseEventHandler } from "react";
-
 import styled from "@emotion/styled";
 
 import CenterBox from "~components/atoms/flexbox/CenterBox";
 
-const FillBox = styled(CenterBox)`
+interface Props {
+  isChecked: boolean;
+}
+
+const Checkbox = styled(CenterBox)<Props>`
   background-color: ${({ theme }) => theme.colors.lightCharcoal};
   width: 35px;
   height: 35px;
   color: ${({ theme }) => theme.colors.darkGray};
   font-size: 35px;
   cursor: pointer;
+  :after {
+    content: "${({ isChecked }) => (isChecked ? "✔" : "")}";
+  }
 `;
-
-interface Props {
-  isChecked: boolean;
-  onClick: MouseEventHandler;
-}
-
-const Checkbox: React.FC<Props> = ({ isChecked, onClick }) => {
-  return <FillBox onClick={onClick}>{isChecked ? "✔" : ""}</FillBox>;
-};
 
 export default Checkbox;
