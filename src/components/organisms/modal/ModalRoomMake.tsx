fe9c6084e9343755fd091738b823b3b452bb7794
vgihan <jum654@naver.com>
2022-03-20 00:01:23 +0900

    fix: LockCheckbox 컴포넌트 molecules로 이동
    - 두 개 이상의 element를 가지기 때문에 atoms가 아니라고 판단함.

--- src/components/organisms/modal/ModalRoomMake.tsx
+++ src/components/organisms/modal/ModalRoomMake.tsx
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 import ButtonRound from "~components/atoms/button/ButtonRound";
-import LockCheckbox from "~components/atoms/checkbox/LockCheckbox";
 import CenterBox from "~components/atoms/flexbox/CenterBox";
 import ModalOff from "~components/atoms/modal/ModalOffButton";
 import ModalTextInput from "~components/atoms/modal/ModalTextInput";
 import TextRighteous from "~components/atoms/text/TextRighteous";
+import LockCheckbox from "~components/molecules/LockCheckbox";
 import useAllCheck from "~hooks/useAllCheck";
 import { useMutateRoom } from "~queries/index";
 import { RoomMode } from "~types/model/RoomElement";
